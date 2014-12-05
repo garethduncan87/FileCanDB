be336337f3db9511eea47355c40ab19f5ddfcc10
Gareth Duncan <gareth.duncan87@gmail.com>
2014-12-05 18:54:02 +0000

    Started on writing the code to save an object (insert) into the database

--- SharpJsonDB/FileSystem.cs
+++ SharpJsonDB/FileSystem.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SharpJsonDB
 {
-    public class Class1
+    public class FileSystem
     {
+       
     }
 }
---
+++ SharpJsonDB/Models/Collection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpJsonDB.Models
+{
+    public class Collection
+    {
+        public string Id { get; set; }
+        public string DatabaseId { get; set; } //Link to the database
+    }
+}
