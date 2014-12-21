a301afa55b5887315505e10fdfe2e83d0ca72f74
Gareth Duncan <gareth.duncan87@gmail.com>
2014-12-21 16:27:46 +0000

    Work on "InsertObject" near to complete

---
+++ SharpJsonDBTests/SharpJsonDBTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpJsonDBTests
+{
+    [TestClass]
+    public class SharpJsonDBTests
+    {
+        [TestMethod]
+        public void InsertObjectTest()
+        {
+            TestObject MyTestObject = new TestObject();
+
+            SharpJsonDB.SharpJsonDB MySharpJsonDB = new SharpJsonDB.SharpJsonDB(@"c:\sharpjsondb");
+
+            string EntryId = MySharpJsonDB.InsertObject(MyTestObject, "Blog", "GarethsBlog");
+            if (string.IsNullOrEmpty(EntryId))
+            {
+                Assert.Fail("No ID returned for newly inserted object");
+            }
+        }
+    }
+}
---
+++ SharpJsonDBTests/TestObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpJsonDBTests
+{
+    class TestObject
+    {
+        public TestObject()
+        {
+            this.Content = "Hello world";
+            this.Created = DateTime.Now;
+            this.Description = "An article about Hello world";
+            this.Title = "This is Hello World";
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string Content { get; set; }
+    }
+}
